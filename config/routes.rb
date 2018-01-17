1ba066f2bca64802b129a48b97da602a5d15d93c
Cernan Bernardo <cernanb@gmail.com>
2018-01-16 17:22:36 -0800

    Add show route for teams resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
 
+  resources :teams, only: [:show]
   root "pages#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
