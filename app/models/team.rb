ede592fc22d2e448ca5295d52f0a0cb96d3d07b9
Cernan Bernardo <cernanb@gmail.com>
2018-01-16 16:59:01 -0800

    Build association between users and teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  has_many :user_teams
+  has_many :players, through: :user_teams, class_name: "User"
 end
--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -1,2 +1,4 @@
 class UserTeam < ApplicationRecord
+  belongs_to :team
+  belongs_to :player, class_name: "User", foreign_key: "player_id"
 end
