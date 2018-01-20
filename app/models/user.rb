7e0ae7a4a6e3d47ec736901e8040566c83e4e9a0
Cernan Bernardo <cernanb@gmail.com>
2018-01-20 00:04:13 -0500

    Build top ten rush and receiver methods in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,33 @@ class User < ApplicationRecord
   has_many :player_stats, foreign_key: "player_id"
   has_many :stats, through: :player_stats
 
+  def self.top_ten_receivers
+    all.sort{|a, b| b.total_rec_yards <=> a.total_rec_yards}[0..9]
+  end
+
+  def self.top_ten_rushers
+    all.sort{|a, b| b.total_rush_yards <=> a.total_rush_yards}[0..9]
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
   def total_rec_yards
     player_stats
       .where("stat_id = ?", 1)
       .sum(:yards)
-    end
+  end
     
-    def total_rush_yards
-      player_stats
-        .where("stat_id = ?", 2)
-        .sum(:yards)
-    end
+  def total_rush_yards
+    player_stats
+      .where("stat_id = ?", 2)
+      .sum(:yards)
+  end
       
-    def total_yards
-      player_stats
-        .sum(:yards)
-    end
+  def total_yards
+    player_stats
+      .sum(:yards)
+  end
+
 end
