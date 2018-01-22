cf5802cea89b1361835594e1547196530f3fada2
Cernan Bernardo <cernanb@gmail.com>
2018-01-21 17:29:20 -0800

    Add association between team and seasons

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ApplicationRecord
   has_many :user_teams
   has_many :players, through: :user_teams, class_name: "User"
   belongs_to :league, optional: true
-  has_many :seasons
+  has_many :season_teams
   has_many :seasons, through: :season_teams
 end
