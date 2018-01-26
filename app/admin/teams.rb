eba2120425223cd61d017cc79578b1d25395131a
Cernan Bernardo <cernanb@gmail.com>
2018-01-25 21:40:22 -0800

    Update active admin for teams

--- app/admin/teams.rb
+++ app/admin/teams.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Team do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name
+permit_params :name, :league_id
 #
 # or
 #
