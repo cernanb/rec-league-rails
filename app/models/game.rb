97bd36c6eeb28787914300ec8ac24105fb853d9c
Cernan Bernardo <cernanb@gmail.com>
2018-02-03 23:33:17 -0800

    Build custom attr writer for Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,15 @@
 class Game < ApplicationRecord
   has_many :player_stats
-  belongs_to :season
   belongs_to :week
 
+  
   has_many :matches
   has_many :teams, through: :matches
+  # accepts_nested_attributes_for :matches
+
+  def matches_attributes=(matches_hash)
+    matches_hash.values.each do |team|
+      self.matches.build(team_id: team[:team_id])
+    end
+  end
 end
